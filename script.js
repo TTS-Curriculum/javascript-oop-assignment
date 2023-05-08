1a1fd77d3728a70fe8e878ec511d04df231ae347
EthanAmato <ethannamato@gmail.com>
2023-05-08 14:16:33 -0400

    student assignment code

--- script.js
+++ script.js
@@ -7,28 +7,3 @@
 // (This is called a 'getter' in Object Oriented Programming)
 
 //If you're stuck, take a look at the example class here for reference: https://www.geeksforgeeks.org/introduction-object-oriented-programming-javascript/#
-
-
-class Car {
-  constructor(make, model, year, color) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-    this.color = color;
-  }
-
-  getMake() {
-    return this.make;
-  }
-
-  getModel() {
-    return this.model;
-  }
-
-  getYear() {
-    return this.year;
-  }
-  getColor() {
-    return this.color;
-  }
-}
